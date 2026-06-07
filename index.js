a9fc021e0e156516dead7af634014e3cfa6f13db
JhoymarSierra <jhoimarbertel@gmail.com>
2026-06-06 20:08:25 -0500

    usar DATABASE_URL

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ app.use(cors());
 
 // ─── CONFIGURA TU BASE DE DATOS AQUÍ ───────────────────────
 const db = new Pool({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port:     process.env.DB_PORT,
-  ssl:      { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 // ────────────────────────────────────────────────────────────
 db.connect()
