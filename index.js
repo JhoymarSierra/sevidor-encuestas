47907a996273fe105a233dda829cd8fed7e097b5
JhoymarSierra <jhoimarbertel@gmail.com>
2026-06-09 17:21:07 -0500

    fix conexion Supabase

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ app.use(cors());
 
 // ─── CONFIGURA TU BASE DE DATOS AQUÍ ───────────────────────
 const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  host:     process.env.DB_HOST,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port:     process.env.DB_PORT,
+  ssl:      { rejectUnauthorized: false }
 });
 // ────────────────────────────────────────────────────────────
 db.connect()
